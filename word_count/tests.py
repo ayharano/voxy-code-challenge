e7ddf62b1554ac25c5fa33e24ad002c74ef3bf1c
Alexandre Harano <email@ayharano.dev>
2020-11-23 09:52:03 -0300

    :lock: CSRF measures implemented

--- word_count/tests.py
+++ word_count/tests.py
@@ -15,25 +15,45 @@ class WordCountViewURLTests(TestCase):
 
 
 class WordCountViewPageStructureTests(TestCase):
-    def test_word_count_page_has_one_form(self):
+    def _selector(self):
         response = self.client.get(reverse('word_count:word_count'))
         content_as_str = response.content.decode(encoding='utf-8')
-        selector = Selector(text=content_as_str)
+        return Selector(text=content_as_str)
+
+    def test_word_count_page_has_one_form(self):
+        selector = self._selector()
         found_node_list = selector.xpath('//form').getall()
         self.assertEqual(len(found_node_list), 1)
 
     def test_word_count_page_has_one_textarea_child_in_form(self):
-        response = self.client.get(reverse('word_count:word_count'))
-        content_as_str = response.content.decode(encoding='utf-8')
-        selector = Selector(text=content_as_str)
+        selector = self._selector()
         xpath_query = '//form//textarea'
         found_node_list = selector.xpath(xpath_query).getall()
         self.assertEqual(len(found_node_list), 1)
 
+    def test_word_count_page_has_two_input_child_in_form(self):
+        selector = self._selector()
+        xpath_query = '//form//input'
+        found_node_list = selector.xpath(xpath_query).getall()
+        self.assertEqual(len(found_node_list), 2)
+
     def test_word_count_page_has_one_input_submit_child_in_form(self):
-        response = self.client.get(reverse('word_count:word_count'))
-        content_as_str = response.content.decode(encoding='utf-8')
-        selector = Selector(text=content_as_str)
+        selector = self._selector()
         xpath_query = '//form//input[@type="submit"]'
         found_node_list = selector.xpath(xpath_query).getall()
         self.assertEqual(len(found_node_list), 1)
+
+    def test_word_count_page_has_one_input_hidden_child_in_form(self):
+        selector = self._selector()
+        xpath_query = '//form//input[@type="hidden"]'
+        found_node_list = selector.xpath(xpath_query).getall()
+        self.assertEqual(len(found_node_list), 1)
+
+    def test_word_count_page_has_csrfmiddlewaretoken(self):
+        selector = self._selector()
+        xpath_query = (
+            '//form//input[@type="hidden" and @name="csrfmiddlewaretoken"]'
+        )
+        found_node_list = selector.xpath(xpath_query).getall()
+        self.assertEqual(len(found_node_list), 1)
+
