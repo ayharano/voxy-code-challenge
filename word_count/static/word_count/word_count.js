87b462708bb5a31b50e13b4b02c07f702adbde92
Alexandre Harano <email@ayharano.dev>
2020-11-23 10:08:22 -0300

    :lipstick: AJAX request and proper form error displayed when user submit empty text

--- word_count/static/word_count/word_count.js
+++ word_count/static/word_count/word_count.js
@@ -1,1 +1,54 @@
 "use strict";
+
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
+
+
+$(document).ready(
+  function() {
+    $( 'textarea#textbox' ).bind(
+      'input',
+      function() {
+        $( 'textarea#textbox.is-invalid' ).removeClass('is-invalid');
+      }
+    );
+
+    $( 'form' ).submit(
+      function(e) {
+        var form = $(this);
+
+        $.ajax({
+          headers: {
+            'X-CSRFToken': csrftoken,
+          },
+          method: 'POST',
+          data: $(form).serialize(),
+        })
+        .fail(
+          function(data) {
+            $( 'textarea#textbox' ).addClass('is-invalid');
+          }
+        );
+
+        e.preventDefault();
+        return false;
+      }
+    );
+  }
+);
