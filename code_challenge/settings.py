f8c61a812a88f6d0e078c8d141ad991a13f15727
Alexandre Harano <email@ayharano.dev>
2020-11-23 09:10:02 -0300

    :heavy_plus_sign: Set app in project settings

--- code_challenge/settings.py
+++ code_challenge/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'word_count.apps.WordCountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
